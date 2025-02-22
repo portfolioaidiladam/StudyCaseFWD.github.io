68f4d2c7de47b24c44cc450f42962bbb519ce86c
portfolioaidiladam <portfolioaidiladam@gmail.com>
2025-02-22 09:33:42 +0700

    first commit

---
+++ src/main/java/com/aidil/game/tictactoe/TicTacToeApplication.java
@@ -0,0 +1,10 @@
+package com.aidil.game.tictactoe;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeApplication.class, args);
+    }
+}
